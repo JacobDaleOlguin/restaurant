19d93135d5bb5a3e749da54de2c90166ca4a2fe8
JacobDaleOlguin <JacobDaleOlguin@gmail.com>
2024-05-16 14:06:32 -0400

    Initial setup of React TypeScript project with Webpack - Configured TypeScript and Webpack - Added basic project structure and essential files - Set up React "^18.3.1" environment

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = { //  this tells Webpack where to start bundling your application code. It points to the index.tsx file in the src directory.
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // this rule applies to all TypeScript files (.tsx and .ts)
+        use: 'babel-loader', // this tells Webpack to use Babel to transpile your TypeScript and potentially other modern JavaScript features 
+                            // (depending on Babel configuration) to a format compatible with older browsers
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/, //same as tsx
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'], // this tells Webpack which extensions to resolve when encountering file imports without extensions 
+                                        // specifying .tsx, .ts, and .js ensures Webpack can find your TypeScript and JavaScript files
+}, //error on this exact line under the brace Expression expected.ts(1109)
+  output: { // this section defines where the bundled code will be placed
+    filename: 'bundle.js', // teh name of the bundled JavaScript file (bundle.js)
+    path: __dirname + '/dist' // the output directory for the bundled files (./dist)
+  },
+  plugins: [ // his section defines plugins that can manipulate the build process or add functionality
+    new HtmlWebPackPlugin({
+      template: './src/index.html' // this plugin injects your index.html file from the src directory into the final bundle
+                                   // it ensures your HTML file references the bundled JavaScript file correctly
+    })
+  ],
+  devServer: { // this configures the built-in development server provided by Webpack
+    static: {
+      directory: path.join(__dirname, 'public'), // this sets the directory for serving static files during development (index html and others)
+    },
+    compress: true, // this enables gzip compression for the development server to improve transfer speeds
+    port: 9000, // this sets the devlopment port
+  },
+};
